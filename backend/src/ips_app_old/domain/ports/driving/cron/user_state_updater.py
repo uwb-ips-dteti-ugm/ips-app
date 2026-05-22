0fe00e59ec3170eb1f39684566e573ac94fe0965
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2026-05-22 10:18:07 +0700

    feat/simplified: (chore) Old app removed.

--- backend/src/ips_app_old/domain/ports/driving/cron/user_state_updater.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class UserStateUpdaterCron(ABC):
-    @abstractmethod
-    async def update_user_states(self) -> None:
-        """Update user states from last-activity duration cutoffs."""
-        ...
--- backend/src/ips_app_old/main.py
+++
@@ -1,4 +0,0 @@
-from ips_app_old.compositions.fastapi import create_app
-
-
-app = create_app()
